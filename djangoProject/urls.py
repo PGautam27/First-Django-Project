1e81476797f5a36fdfd1249885ca8632cbe2aa1d
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-04-30 23:34:13 +0530

    First Django

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Gautam Ice Cream Admin"
+admin.site.site_title = "Gautam Ice Cream Admin Portal"
+admin.site.index_title = "Welcome to Gautam Ice Cream Admin Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myApp.urls'))
