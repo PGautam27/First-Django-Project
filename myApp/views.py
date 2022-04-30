14c6477e920c06599bf2314030cf81ad48e5d702
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-04-30 22:58:22 +0530

    First Django

--- myApp/views.py
+++ myApp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("this is homepage")
+    return render(request, 'index.html')
 
 
 def about(request):
