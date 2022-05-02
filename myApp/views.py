a16aa8cc3d90e0750fce7a19e55fbe101dae7cd0
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-05-02 18:03:15 +0530

    First Django

--- myApp/views.py
+++ myApp/views.py
@@ -1,6 +1,8 @@
+from django.db.utils import IntegrityError
 from django.shortcuts import render
 from datetime import datetime
 from myApp.models import Contact
+
 
 # Create your views here.
 def index(request):
@@ -20,9 +22,14 @@ def services(request):
 
 def contact(request):
     if request.method == "POST":
+        name = request.POST.get('name')
         email = request.POST.get('email')
         phone = request.POST.get('phone')
         desc = request.POST.get('desc')
-        contacts = Contact(email=email, phone=phone, desc=desc, date=datetime.today())
-        contacts.save()
+        contacts = Contact(name=name, email=email, phone=phone, desc=desc, date=datetime.today())
+        try:
+            contacts.save()
+        except IntegrityError as err:
+            print("Ignoring unhandled integrity error:")
+            print(err)
     return render(request, 'contact.html')
