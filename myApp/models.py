353105877177db33f57fbeff67b462b115d593dd
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-05-03 10:34:29 +0530

    First Django

--- myApp/models.py
+++ myApp/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
-    name = models.CharField(max_length=122)
-    email = models.CharField(max_length=122)
-    phone = models.CharField(max_length=12)
-    desc = models.TextField()
+    name = models.CharField(max_length=122, null=False)
+    email = models.CharField(max_length=122, null=True)
+    phone = models.CharField(max_length=12, null=True)
+    desc = models.TextField(null=True)
     date = models.DateField()
