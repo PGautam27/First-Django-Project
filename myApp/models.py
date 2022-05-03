b2a3337340cb1eb00c99ca9e5549d396a914f4bd
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-05-03 11:49:25 +0530

    First Django

--- myApp/models.py
+++ myApp/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
-    name = models.CharField(max_length=122)
-    email = models.CharField(max_length=122)
-    phone = models.CharField(max_length=12)
-    desc = models.TextField()
+    name = models.CharField(max_length=122, null=True, default="gautam")
+    email = models.CharField(max_length=122, null=True, default="gautam@gmail.com")
+    phone = models.CharField(max_length=10, null=True, default=1234567895)
+    desc = models.TextField(null=True)
     date = models.DateField()
